afb50bacff34ea0f85e44eaf3c43f28fe9256663
Gautam Bisht <bishtgautam012@gmail.com>
2024-04-07 20:18:04 +0530

    next js and tailwind css setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <div className="flex  justify-between items-center"></div>
+      </div>
+    </>
+  );
+};
+
+export default Header;

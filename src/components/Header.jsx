b4100c2307716f5eac35f9830177d729e90ee3de
Gautam Bisht <bishtgautam012@gmail.com>
2024-04-07 20:37:20 +0530

    Header component added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <>
-      <div>
-        <div className="flex  justify-between items-center"></div>
+      <div className="sticky top-0 shadow-sm border-b bg-white z-30 p-3 ">
+        <div className="max-w-[75%]  mx-auto flex  justify-between items-center">
+          <Link href="/">
+            <Image
+              className="hidden lg:inline-flex"
+              width={90}
+              height={90}
+              src="/instagram-Logo.png"
+              alt="instagram logo"
+            ></Image>
+            <Image
+              className="lg:hidden"
+              width={40}
+              height={40}
+              src="/instagram_PNG.png"
+              alt="instagram logo"
+            ></Image>
+          </Link>
+
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full py-2 px-4 max-w-[210px] bg-gray-50 border border-gray-200 rounded text-sm"
+          />
+
+          <button className="text-sm font-semibold text-blue-500 ">
+            Log In
+          </button>
+        </div>
       </div>
     </>
   );
